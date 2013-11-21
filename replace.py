278a1fb5d099c190af8d32be3c2db48e665e07e9
Stephen Calnan <asciimo@gmail.com>
2013-11-20 17:06:08 -0800

    Added recursive directory descent, support for file of regex patterns to ignore.

--- replace.py
+++ replace.py
@@ -8,16 +8,17 @@ import getopt
 """ Globals """
 verbose = False
 recursive = False
+ignore_patterns = []
 
 def main():
 
-    global verbose, recursive
+    global verbose, recursive, ignore_patterns
 
     directory = "."
 
     try:
         opts, args = getopt.getopt(sys.argv[1:], "", ["help", "directory=",
-        "verbose", "recursive"])
+        "ignore=", "verbose", "recursive"])
     except getopt.GetoptError as err:
         # print help information and exit
         print str(err)
@@ -31,6 +32,8 @@ def main():
             recursive = True
         elif o == "--verbose":
             verbose = True
+        elif o == "--ignore":
+            init_ignore_patterns(a)
         elif o == "--help":
             usage()
             sys.exit(0)
@@ -39,25 +42,55 @@ def main():
 
 
 def usage():
-    print("usage: cdn_update.py [--directory=<directory>] [--recursive] [--help] [--verbose]");
+    print("usage: cdn_update.py [--directory=<directory>] [--ignore=<filename>] [--recursive] [--help] [--verbose]");
+
+def init_ignore_patterns(filename):
+    """Compiles each pattern in filename into a regular expression,
+    and stores the results in the global ignore_patterns list."""
+    global ignore_patterns
+    empty_line = re.compile('^\s*$')
+    comment_line = re.compile('^\s*#')
+    try:
+        with open(filename) as f:
+            for line in f:
+                if(empty_line.match(line) or comment_line.match(line)):
+                    continue
+                ignore_patterns.append(re.compile(line))
+    except (IOError):
+        print "Unable to open %s" % filename
+        sys.exit();
 
 def get_files(directory):
 
-    global verbose, recursive
+    global verbose, recursive, ignore_patterns
 
     for root, subdirs, files in os.walk(directory):
-        if verbose:
-            print "Descending into %s" % root
         for filename in files:
-            print "Filename: %s" % os.path.join(root, filename)
-            replace(filename)
+            path = os.path.join(root, filename)
+            for pattern in ignore_patterns:
+                if pattern.match(path):
+                    if verbose:
+                        print "Ignoring file: s%" % path
+                    continue
+            if verbose:
+                print "Processing: %s" % path
+            replacements = replace(filename)
+            if verbose:
+                print "Replaced %d instances in %s" % (replacements, path)
         for subdir in subdirs:
-            print "Subdirectory: %s" % os.path.join(root, subdir)
-        if recursive == False:
-            return
+            path = os.path.join(root, subdir)
+            for pattern in ignore_patterns:
+                if pattern.match(path):
+                    if verbose:
+                        print "Ignoring directory: s%" % path
+                    continue
+                if verbose:
+                    print "Subdirectory: %s" % path
+                if recursive:
+                    get_files(path)
 
 def replace(filename):
-    return
+    return 3
 
 if __name__ == "__main__":
     main()
